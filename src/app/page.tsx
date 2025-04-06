7f85f650abf28e9782e5aa74e5f34a7fea30e409
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-06 22:24:23 +0530

    feat: add navigation header with links to homepage
    Introduce a navigation header with links to various sections of the website, including themed jewelry, all categories, and support. This enhances the user experience by providing easy access to different parts of the site.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
 export default function Home() {
   return (
-    <p>Hello</p>
+    <>
+      <header>
+        <nav>
+          <a href="#">Laniakea</a>
+          <a href="/themed-jewelery">Themed Jewelry</a>
+          <a href="/all-categories">All Categories</a>
+          <a href="/customized">Customized</a>
+          <a href="/explore">Explore</a>
+          <a href="/support">Support</a>
+          <a href="/search">search-svg</a>
+          <a href="/cart">cart-svg</a>
+          <a href="/language">language-svg</a>
+          <a href="/sign-in">
+            <button type="button">person-svg Sign in</button>
+          </a>
+        </nav>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
