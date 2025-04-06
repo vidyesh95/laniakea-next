f2477fc415b7e02235b4bb2e3431d38be69accdc
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-06 21:37:46 +0530

    feat(fonts): add Roboto and Roboto Flex fonts to the project
    This commit introduces Roboto and Roboto Flex fonts by creating a new fonts.ts file, importing them into the layout, and updating globals.css with the new font variables. Additionally, the page.tsx file is simplified to display a "Hello" message for testing purposes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { roboto, robotoFlex } from "@/app/ui/fonts";
 import "./globals.css";
 
 const geistSans = Geist({
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Roboto, Roboto_Flex } from "next/font/google";
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "500",
+  display: "swap",
+});
+
+
+export const robotoFlex = Roboto_Flex({
+  subsets: ["latin"],
+  weight: "700",
+  display: "swap",
+});
