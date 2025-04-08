e05ee878e007800db512ecd9ce56d5493dfaa58b
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-08 17:03:49 +0530

    style(notification-banner): update styles and add hover effect
    Updated the background colors of the banner and ad container for better visual distinction. Added a hover effect to the close button to improve user interaction. Commented out unused state logic for future reference.

--- src/app/ui/components/core/notification-banner.tsx
+++ src/app/ui/components/core/notification-banner.tsx
@@ -1,16 +1,25 @@
+// import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function NotificationBanner() {
+  // const [isCollapsed, setIsCollapsed] = useState(false);
+
+  // if (isCollapsed) {
+  //   return (
+  //    null 
+  //   ) 
+  // }
+
   return (
     // collapsing_banner
     <section className="w-full bg-white flex justify-center">
       {/* banner_container */}
-      <div className="w-[1024px] flex ps-6 pe-4 gap-2">
+      <div className="w-[1024px] flex ps-6 pe-4 gap-2 bg-green-500">
         {/* banner_ad */}
         <Link
           href="https://play.google.com/"
-          className="grow flex flex-wrap gap-1 justify-center items-center"
+          className="grow flex flex-wrap gap-1 justify-center items-center bg-amber-500"
         >
           <p className="text-sm">
             <span className="text-black">
@@ -22,7 +31,7 @@ export default function NotificationBanner() {
           </p>
         </Link>
         {/* close_bannerbutton */}
-        <button type="button" className="p-2 grow-0">
+        <button type="button" className="p-2 grow-0 hover:cursor-pointer">
           <Image
             src="/svg/core/close_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg"
             width={24}
