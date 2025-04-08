cca030cf1a16ab912b72b242b10b2e9877431c3d
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-08 15:30:23 +0530

    feat(notification-banner): implement notification banner with close button
    Add a notification banner component that includes a link to download the app and a close button. The banner is styled to be responsive and centered. The close button uses a new SVG icon for better visual consistency. Additionally, update the page layout to include the notification banner in the header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,44 @@
-import NotificationBanner from "./ui/components/core/notification-banner"
+import NotificationBanner from "./ui/components/core/notification-banner";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="bg-red-500">
         <NotificationBanner />
+        {/* navigation_bar */}
         <nav>
-          <a href="#">Laniakea</a>
-          <a href="/themed-jewelery">Themed Jewelry</a>
-          <a href="/all-categories">All Categories</a>
-          <a href="/customized">Customized</a>
-          <a href="/explore">Explore</a>
-          <a href="/support">Support</a>
-          <a href="/search">search-svg</a>
-          <a href="/cart">cart-svg</a>
-          <a href="/language">language-svg</a>
-          <a href="/sign-in">
-            <button type="button">person-svg Sign in</button>
-          </a>
+          {/* navigation_container */}
+          <div>
+            {/* logo */}
+            <a href="#" className="bg-blue-500 inline-block">
+              <Image
+                src="/svg/core/logo.svg"
+                width={133}
+                height={44}
+                alt="Laniakea"
+              />
+            </a>
+
+            {/* tabs */}
+            <div className="bg-blue-500 inline-block">
+              <a href="/themed-jewelery">Themed Jewelry</a>
+              <a href="/all-categories">All Categories</a>
+              <a href="/customized">Customized</a>
+              <a href="/explore">Explore</a>
+              <a href="/support">Support</a>
+            </div>
+
+            {/* buttons */}
+            <div className="bg-blue-500 inline-block">
+              <a href="/search">search-svg</a>
+              <a href="/cart">cart-svg</a>
+              <a href="/language">language-svg</a>
+              <a href="/sign-in">
+                <button type="button">person-svg Sign in</button>
+              </a>
+            </div>
+          </div>
         </nav>
       </header>
       <main></main>
--- src/app/ui/components/core/notification-banner.tsx
+++ src/app/ui/components/core/notification-banner.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotificationBanner() {
   return (
-    <section>
-        <div></div>
+    // collapsing_banner
+    <section className="w-full bg-white flex justify-center">
+      {/* banner_container */}
+      <div className="w-[1024px] flex ps-6 pe-4 gap-2">
+        {/* banner_ad */}
+        <Link
+          href="https://play.google.com/"
+          className="grow flex flex-wrap gap-1 justify-center items-center"
+        >
+          <p className="text-sm">
+            <span className="text-black">
+              Download the Laniakea Store app available both on android and
+              ios.&nbsp;
+            </span>
+            <span className="text-[#9C4330] underline">Download the App</span>
+            <span className="text-[#9C4330]">&nbsp;&gt;</span>
+          </p>
+        </Link>
+        {/* close_bannerbutton */}
+        <button type="button" className="p-2 grow-0">
+          <Image
+            src="/svg/core/close_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg"
+            width={24}
+            height={24}
+            alt="Close banner"
+          />
+        </button>
+      </div>
     </section>
-  )
+  );
 }
